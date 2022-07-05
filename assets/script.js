3ffffe4f2e41a2cbc41e4e9e5e86461c0b87c24d
Ryan <thisianemailaddressthatis@gmail.com>
2022-07-05 15:26:06 -0500

    second commit

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,9 @@
 // Assignment Code
+// selects the generate button
 var generateBtn = document.querySelector("#generate");
 
 // Arrays for all the characters to be put in the password. One for special charcters and one for numbers, upper, and lowercase letters.
-specialChar = [
+special = [
   "!",
   "#",
   "$",
@@ -36,7 +37,7 @@ specialChar = [
   "~",
 ];
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-lowerCase = [
+letters = [
   "a",
   "b",
   "c",
@@ -64,7 +65,7 @@ lowerCase = [
   "y",
   "z",
 ];
-upperCase = [
+upperLetters = [
   "A",
   "B",
   "C",
@@ -93,25 +94,86 @@ upperCase = [
   "Z",
 ];
 
-var choice;
 // Function that starts when the generate button is clicked on.
 function generatePassword() {
   input = parseInt(
     prompt(
-      "How many characters do you an the password to be? Must be between 8 and 128"
+      "How many characters do you want your password to be. Must be a number between 8 and 128 characters."
     )
   );
+  // First if statement containg confirms that will decide what characters are in password.
   if (input < 8 || input > 128) {
-    enter = parseInt(prompt("You must choose between 8 and 128"));
+    input = parseInt(
+      prompt("The password must be between 8 and 128 characters.")
+    );
+  } else if (!input) {
+    alert("You must enter a number.");
+    return;
   } else {
-    confirmNumber = confirm("Do you want numbers?");
-    confirmCharacter = confirm("Do you want special characters?");
-    confirmUppercase = confirm("Do you want uppercase letters?");
+    confirmSpecial = confirm("Do you want special characters?");
+    confirmNumbers = confirm("Do you want numbers?");
     confirmLowercase = confirm("Do you want lowercase letters?");
+    confirmUppercase = confirm("Do you want uppercase letters?");
   }
+  // If statements for the different combinations of decisions made in the confirms.
+  if (
+    confirmSpecial &&
+    confirmNumbers &&
+    confirmUppercase &&
+    confirmLowercase
+  ) {
+    decision = special.concat(numbers, letters, upperLetters);
+  } else if (confirmSpecial && confirmNumbers && confirmUppercase) {
+    decision = special.concat(numbers, upperLetters);
+  } else if (confirmSpecial && confirmNumbers && confirmLowercase) {
+    decision = special.concat(numbers, letters);
+  } else if (confirmSpecial && confirmLowercase && confirmUppercase) {
+    decision = special.concat(letters, upperLetters);
+  } else if (confirmNumbers && confirmLowercase && confirmUppercase) {
+    decision = numbers.concat(letters, upperLetters);
+  } else if (confirmSpecial && confirmNumbers) {
+    decision = special.concat(numbers);
+  } else if (confirmSpecial && confirmLowercase) {
+    decision = special.concat(letters);
+  } else if (confirmSpecial && confirmUppercase) {
+    decision = special.concat(upperLetters);
+  } else if (confirmLowercase && confirmNumbers) {
+    decision = letters.concat(numbers);
+  } else if (confirmLowercase && confirmUppercase) {
+    decision = letters.concat(upperLetters);
+  } else if (confirmNumbers && confirmUppercase) {
+    decision = numbers.concat(upperLetters);
+  } else if (confirmSpecial) {
+    decision = special;
+  } else if (confirmNumbers) {
+    decision = numbers;
+  } else if (confirmLowercase) {
+    decision = letters;
+  } else if (confirmUppercase) {
+    decision = upperLetters;
+  } else if (
+    !confirmSpecial &&
+    !confirmNumbers &&
+    !confirmUppercase &&
+    !confirmLowercase
+  ) {
+    decision = alert("Please choose at least one parameter.");
+  }
+  // makes password an array so it can hold all the characters
+  var password = [];
+
+  // for loop that randomizes the array from the decisions made in the confirms
+  for (var i = 0; i < input; i++) {
+    var randomChoice = decision[Math.floor(Math.random() * decision.length)];
+    password.push(randomChoice);
+  }
+  // .join makes the password variable a string so it can be displayed in the text box
+  var ps = password.join("");
+  // returns this string to the writePassword function.
+  return ps;
 }
 
-// Write password to the #password input
+// Starts the generate password function and takes returned value and puts it in #password.
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -119,5 +181,5 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// Add event listener to generate button
+// Starts the write password function when genarate button is lciked on.
 generateBtn.addEventListener("click", writePassword);
